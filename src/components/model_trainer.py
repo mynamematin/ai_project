870d14f3741e2edfe7f5c542421b5b7c27b88542
mynamematin <matinahmed000@gmail.com>
2023-04-01 00:23:56 +0530

    prediction pipeline

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -21,12 +21,12 @@ from src.utils import save_object,evaluate_models
 
 @dataclass
 class ModelTrainerConfig:
-    trained_model_file_path=os.path.join("artifacts","model.pkl")
+    trained_model_file_path=os.path.join('artifacts',"model.pkl")
+    
 
 class ModelTrainer:
     def __init__(self):
         self.model_trainer_config=ModelTrainerConfig()
-
 
     def initiate_model_trainer(self,train_array,test_array):
         try:
@@ -46,6 +46,7 @@ class ModelTrainer:
                 "CatBoosting Regressor": CatBoostRegressor(verbose=False),
                 "AdaBoost Regressor": AdaBoostRegressor(),
             }
+            
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
@@ -83,12 +84,15 @@ class ModelTrainer:
                 }
                 
             }
+            
 
             model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
                                              models=models,param=params)
             
+            
             ## To get best model score from dict
             best_model_score = max(sorted(model_report.values()))
+            
 
             ## To get best model name from dict
 
@@ -96,9 +100,10 @@ class ModelTrainer:
                 list(model_report.values()).index(best_model_score)
             ]
             best_model = models[best_model_name]
+            logging.info("read10")
 
             if best_model_score<0.6:
-                raise CustomException("No best model found")
+                raise CustomerException("No best model found")
             logging.info(f"Best found model on both training and testing dataset")
 
             save_object(
@@ -111,9 +116,10 @@ class ModelTrainer:
             r2_square = r2_score(y_test, predicted)
             return r2_square
             
-
-
-
             
         except Exception as e:
-            raise CustomException(e,sys)
+            raise CustomerException(e,sys)
+
+
+
+
